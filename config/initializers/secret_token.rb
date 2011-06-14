ccd86a1bacb20e6993611beb01b7f8656a7e6ade
Hemant Kumar <hkumar@crri.co.in>
2011-06-15 00:18:34 +0530

    add crap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TryUpload::Application.config.secret_token = '4b842124c77cfd151af7a289c804f9d84702e0a1e1443e13f9f287c50eac25a414d2b6f3b30d8990598fa561e9ff3701cba536ace28f2ea8e210fa4deef57153'
